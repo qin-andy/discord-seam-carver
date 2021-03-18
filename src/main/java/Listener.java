3f7c66098d20750ae9ba29dfbcd56594e53c6705
andyq <Andy>
2021-03-18 15:57:57 -0700

    Moved ListenerAdapter to Listener class

---
+++ src/main/java/Listener.java
@@ -0,0 +1,54 @@
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.*;
+public class Listener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        // if (event.getAuthor().isBot()) return;
+        System.out.println("Message from " +
+                event.getAuthor().getName() +
+                ": " +
+                event.getMessage()
+        );
+        MessageChannel channel = event.getChannel();
+        if (event.getAuthor().isBot()) return;
+        if (event.getMessage().getContentRaw().equals("clean up, kibby")) {
+            deleteFromUser(event.getChannel(), event.getJDA().getSelfUser(), 5);
+            channel.sendMessage("SMOOO!! (cleaning)").queue();
+            return;
+        }
+        channel.sendMessage(genResponse()).queue();
+    }
+
+    private String genResponse() {
+        String msg = "";
+        double random = Math.random();
+        if (random < 0.33) {
+            msg = "smo.";
+        } else if (random >= 0.33 && random < 0.66) {
+            msg = "smoh!";
+        } else if (random >= 0.66 && random < 0.9) {
+            msg = "smoh....";
+        } else if (random >= 0.9 && random < 0.95) {
+            msg = "SMOH!!?";
+        } else {
+            msg = "SMIMAMO!!! SMIMAMO!!! (doing a rare kibby dance)";
+        }
+        return msg;
+    }
+
+    private void deleteFromUser(MessageChannel channel, User author, int amount) {
+        List<Message> messages = new ArrayList<>();
+        channel.getIterableHistory()
+                .forEachAsync(m -> {
+                    if (m.getAuthor().equals(author)) messages.add(m);
+                    return messages.size() < amount;
+                })
+                .thenRun(() -> channel.purgeMessages(messages));
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,59 +10,13 @@ import java.util.*;
 
 import javax.security.auth.login.LoginException;
 
-public class Main extends ListenerAdapter {
+public class Main {
     public static void main(String[] args)
             throws LoginException, InterruptedException
     {
         String token = "ODE3MjgwNjk2ODI4NTU5Mzgw.YEHN9g.xX6mfDGlUOIcnMCdDq0JIQD0_pc";
         JDABuilder jda = JDABuilder.createDefault(token)
-                .addEventListeners(new Main());
+                .addEventListeners(new Listener());
         jda.build();
-
-    }
-
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        // if (event.getAuthor().isBot()) return;
-        System.out.println("Message from " +
-                event.getAuthor().getName() +
-                ": " +
-                event.getMessage()
-        );
-        MessageChannel channel = event.getChannel();
-        if (event.getAuthor().isBot()) return;
-        if (event.getMessage().getContentRaw().equals("clean up, kibby")) {
-            deleteFromUser(event.getChannel(), event.getJDA().getSelfUser(), 5);
-            channel.sendMessage("SMOOO!! (cleaning)").queue();
-            return;
-        }
-        channel.sendMessage(genResponse()).queue();
-    }
-
-    private String genResponse() {
-        String msg = "";
-        double random = Math.random();
-        if (random < 0.33) {
-            msg = "smo.";
-        } else if (random >= 0.33 && random < 0.66) {
-            msg = "smoh!";
-        } else if (random >= 0.66 && random < 0.9) {
-            msg = "smoh....";
-        } else if (random >= 0.9 && random < 0.95) {
-            msg = "SMOH!!?";
-        } else {
-            msg = "SMIMAMO!!! SMIMAMO!!! (doing a rare kibby dance)";
-        }
-        return msg;
-    }
-
-    private void deleteFromUser(MessageChannel channel, User author, int amount) {
-        List<Message> messages = new ArrayList<>();
-        channel.getIterableHistory()
-                .forEachAsync(m -> {
-                    if (m.getAuthor().equals(author)) messages.add(m);
-                    return messages.size() < amount;
-                })
-                .thenRun(() -> channel.purgeMessages(messages));
     }
 }
