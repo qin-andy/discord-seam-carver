8511e61d9215f3a24e509ff74309d22c4e8169bc
andyq <andyqin17@gmail.com>
2021-03-23 19:05:56 -0700

    Linked listener to Carver class to carve user attachments

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -5,7 +5,10 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+
 public class Listener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -15,9 +18,24 @@ public class Listener extends ListenerAdapter {
                 ": " +
                 event.getMessage()
         );
-            String fileName = event.getMessage().getAttachments().get(0).getFileName();
-            System.out.println("Received " + fileName + " from " + event.getAuthor().getName());
-            event.getMessage().getAttachments().get(0).downloadToFile("src/main/resources/images/" + fileName);
-            System.out.println("Smohohmo! (saved)");
+        String fileName = event.getMessage().getAttachments().get(0).getFileName();
+        System.out.println("Received " + fileName + " from " + event.getAuthor().getName());
+
+        try {
+            event.getMessage().getAttachments().get(0).downloadToFile("src/main/resources/images/" + fileName).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Smohohmo! (saved)");
+        Carver carver = new Carver();
+        try {
+            System.out.println(fileName);
+            carver.createEnergyArray("src/main/resources/images/" + fileName);
+        } catch (IOException e) { //Check this later
+            System.out.print("File not found!");
+        }
     }
 }
