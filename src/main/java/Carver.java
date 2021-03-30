615fe92f73e442b26e249c69ce41d98765301e95
andyq <andyqin17@gmail.com>
2021-03-30 13:19:54 -0700

    changed all 2d arrays to 1d arrays

--- src/main/java/Carver.java
+++ src/main/java/Carver.java
@@ -19,116 +19,6 @@ public class Carver {
         carver.carve("src/main/resources/images/lapp.png", 200, 0);
     }
 
-
-    // Creates an energy map from a filepath to an image
-    public int carve(String filePath, int cutSize) throws IOException {
-
-        // Reading file using ImageIO read
-        File file = new File(filePath);
-        BufferedImage image;
-        try {
-            image = ImageIO.read(file);
-        } catch (IOException e) {
-            return -2;
-        }
-
-        int height = image.getHeight();
-        int width = image.getWidth();
-        System.out.println("Image size is " + width + " by " + height);
-
-        // compression
-        int maxSize = 1500;
-        if (height > maxSize || width > maxSize) {
-            long start = System.currentTimeMillis();
-            double scale = (double) maxSize / Math.max(height, width);
-
-            int newW = (int) (width*scale);
-            int newH = (int) (height*scale);
-            Image scaled = image.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-            BufferedImage scaledImage = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-            scaledImage.createGraphics().drawImage(scaled, 0, 0, null);
-            scaledImage.createGraphics().dispose(); //TODO: CHECK IF THIS IS ACTUALLY DISPOSED!
-            image = scaledImage;
-            System.out.println("Image too large! Scaling down to " + newW + " by " + newH);
-
-            double cutFactor = (double) cutSize / width;
-            System.out.println("Adjusted cut size from " + cutSize + " to " + (int) (cutFactor * newW));
-            cutSize = (int) (cutFactor * newW);
-
-            long end = System.currentTimeMillis();
-            System.out.println("Compression took " + (end - start) + "ms!");
-
-        }
-
-        // Conversion to ARGB
-        BufferedImage imageARGB = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        imageARGB.createGraphics().drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
-
-
-        imageARGB.createGraphics().dispose();
-        image = imageARGB;
-
-
-        // Determining cut is possible (size)
-        if (cutSize > image.getWidth()) {
-            System.out.println("Cut size too big!");
-            return -1;
-        }
-
-        // Timing
-        int convertToRGBTime = 0;
-        int energyMapTime = 0;
-        int shortestSeamTime = 0;
-        int pathRemovalTime = 0;
-        long iterationStartTime = System.currentTimeMillis();
-
-
-        int iWidth = image.getWidth();
-
-        for (int i = 0; i < cutSize; i++) {
-            long startTime = System.currentTimeMillis();
-            //int[][] imageRGB = convertToRGB3(image);
-            int[][] imageRGB = convertToRGB(image, iWidth);
-            long endTime = System.currentTimeMillis();
-            convertToRGBTime += (endTime - startTime);
-
-            startTime = System.currentTimeMillis();
-            int[][] energyMap = createEnergyMap(imageRGB);
-            endTime = System.currentTimeMillis();
-            energyMapTime += (endTime - startTime);
-
-            startTime = System.currentTimeMillis();
-            int[] path = shortestPath(energyMap);
-            endTime = System.currentTimeMillis();
-            shortestSeamTime += (endTime - startTime);
-
-            startTime = System.currentTimeMillis();
-            image = removePath(path, image, iWidth);
-            endTime = System.currentTimeMillis();
-            pathRemovalTime += (endTime - startTime);
-
-            //Timing for iterations
-            if (i%25 == 0) {
-                long iterationEndTime = System.currentTimeMillis();
-                System.out.println("Iteration " + i + " took " + (iterationEndTime - iterationStartTime) + " milliseconds");
-                iterationStartTime = System.currentTimeMillis();
-            }
-        }
-
-        System.out.println("For a new image:");
-        System.out.println("Convert to RGB Time: " + convertToRGBTime + " ms");
-        System.out.println("Energy mapping Time: " + energyMapTime + " ms");
-        System.out.println("Path identification Time: " + shortestSeamTime + " ms");
-        System.out.println("Path removal Time: " + pathRemovalTime + " ms");
-        int totalTime = convertToRGBTime + energyMapTime + shortestSeamTime + pathRemovalTime;
-        System.out.println("TOTAL TIME:" + totalTime + " ms");
-
-
-        File outputFile = new File("src/main/resources/images/carved.PNG");
-        ImageIO.write(image, "PNG", outputFile);
-        return totalTime;
-    }
-
     public int carve(String filePath, int cutSize, int cutSizeY) throws IOException {
 
         // Reading file using ImageIO read
@@ -151,7 +41,7 @@ public class Carver {
         }
 
         // compression
-        int maxSize = 1000;
+        int maxSize = 1000; //Integer.MAX_VALUE;
         if (height > maxSize || width > maxSize) {
             long start = System.currentTimeMillis();
             double scale = (double) maxSize / Math.max(height, width);
@@ -205,19 +95,23 @@ public class Carver {
         int iWidth = image.getWidth();
 
         for (int i = 0; i < cutSize; i++) {
+
+            width = image.getWidth();
+            height = image.getHeight();
+
             long startTime = System.currentTimeMillis();
             //int[][] imageRGB = convertToRGB3(image);
-            int[][] imageRGB = convertToRGB(image, iWidth);
+            int[] imageRGB = convertToRGB(image, iWidth);
             long endTime = System.currentTimeMillis();
             convertToRGBTime += (endTime - startTime);
 
             startTime = System.currentTimeMillis();
-            int[][] energyMap = createEnergyMap(imageRGB);
+            int[] energyMap = createEnergyMap(imageRGB, width, height);
             endTime = System.currentTimeMillis();
             energyMapTime += (endTime - startTime);
 
             startTime = System.currentTimeMillis();
-            int[] path = shortestPath(energyMap);
+            int[] path = shortestPath(energyMap, width, height);
             endTime = System.currentTimeMillis();
             shortestSeamTime += (endTime - startTime);
 
@@ -252,19 +146,23 @@ public class Carver {
             System.out.println("Tranposing took  " + (end - start) + "ms!");
             iWidth = image.getWidth();
             for (int i = 0; i < cutSizeY; i++) {
+
+                width = image.getWidth();
+                height = image.getHeight();
+
                 long startTime = System.currentTimeMillis();
                 //int[][] imageRGB = convertToRGB3(image);
-                int[][] imageRGB = convertToRGB(image, iWidth);
+                int[] imageRGB = convertToRGB(image, iWidth);
                 long endTime = System.currentTimeMillis();
                 convertToRGBTime += (endTime - startTime);
 
                 startTime = System.currentTimeMillis();
-                int[][] energyMap = createEnergyMap(imageRGB);
+                int[] energyMap = createEnergyMap(imageRGB, width, height);
                 endTime = System.currentTimeMillis();
                 energyMapTime += (endTime - startTime);
 
                 startTime = System.currentTimeMillis();
-                int[] path = shortestPath(energyMap);
+                int[] path = shortestPath(energyMap, width, height);
                 endTime = System.currentTimeMillis();
                 shortestSeamTime += (endTime - startTime);
 
@@ -272,6 +170,9 @@ public class Carver {
                 image = removePath(path, image, iWidth);
                 endTime = System.currentTimeMillis();
                 pathRemovalTime += (endTime - startTime);
+
+                width = image.getWidth();
+                height = image.getHeight();
 
                 //Timing for iterations
                 if (i%50 == 0) {
@@ -308,23 +209,21 @@ public class Carver {
 
     // I guess when subimages are taken in remove path, the underlying databuffer dimensions aren't changed.
     // we account for that by storing the intiial width as iWidth and using it to map 1d array to 2d
-    private int[][] convertToRGB(BufferedImage image, int iWidth) {
+    private int[] convertToRGB(BufferedImage image, int iWidth) {
         int width = image.getWidth();
         int height = image.getHeight();
-        int[][] result = new int[width][height];
+        int[] result = new int[width*height];
         int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)  {
-                result[x][y] = pixels[y*iWidth + x];
+                result[y*width + x] = pixels[y*iWidth + x];
             }
         }
         return result;
     }
 
-    private int[][] createEnergyMap(int[][] colorArray) {
-        int width = colorArray.length;
-        int height = colorArray[0].length;
-        int[][] energyArray = new int[width][height];
+    private int[] createEnergyMap(int[] colorArray, int width, int height) {
+        int[] energyArray = new int[width*height];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)  {
                 // Typed out for readability
@@ -334,16 +233,15 @@ public class Carver {
 
                 // Edges are trated as adjacent to the opposite side
                 if (x == 0) {
-                    prev = colorArray[width-1][y];
-                    next = colorArray[x+1][y];
+                    prev = colorArray[y*width + (width-1)];
+                    next = colorArray[y*width + x+1];
                 } else if (x == width - 1) {
-                    prev = colorArray[x-1][y];
-                    next = colorArray[0][y];
+                    prev = colorArray[y*width + x - 1];
+                    next = colorArray[y*width];
                 } else {
-                    prev = colorArray[x-1][y];
-                    next = colorArray[x+1][y];
+                    prev = colorArray[y*width + x-1];
+                    next = colorArray[y*width + x+1];
                 }
-
 
                 int pB = prev & 0xFF;
                 int pG = (prev & 0xFF) >> 8;
@@ -360,14 +258,15 @@ public class Carver {
                 int xDeltaSquare = deltaR + deltaG + deltaB;
 
                 if (y == 0) {
-                    prev = colorArray[x][height-1];
-                    next = colorArray[x][y+1];
+
+                    prev = colorArray[(height-1)*width + x];
+                    next = colorArray[width*(y+1) + x];
                 } else if (y == height - 1) {
-                    prev = colorArray[x][y-1];
-                    next = colorArray[x][0];
+                    prev = colorArray[(y-1)*width + x];
+                    next = colorArray[x];
                 } else {
-                    prev = colorArray[x][y];
-                    next = colorArray[x][y];
+                    prev = colorArray[(y-1)*width + x];
+                    next = colorArray[(y+1)*width + x];
                 }
 
                 pB = prev & 0xFF;
@@ -383,23 +282,22 @@ public class Carver {
                 deltaB = (int) Math.pow(pB - nB, 2);
 
                 int yDeltaSquare = deltaR + deltaG + deltaB;
-                energyArray[x][y] = xDeltaSquare + yDeltaSquare;
+                energyArray[y*width + x] = xDeltaSquare + yDeltaSquare;
             }
         }
 
         return energyArray;
     }
 
-    private int[] shortestPath(int[][] energyArray) {
-        int width = energyArray.length;
-        int height = energyArray[0].length;
+
+    private int[] shortestPath(int[] energyArray, int width, int height) {
 
         // Each [x][y] pair is modelled as a node
-        int[][] parent = new int[width][height]; // The x value of the node's parent; the y is the child y - 1
-        int[][] distTo = new int[width][height]; // The shortest distance to the node
+        int[] parent = new int[width*height]; // The x value of the node's parent; the y is the child y - 1
+        int[] distTo = new int[width*height]; // The shortest distance to the node
+        Arrays.fill(distTo, Integer.MAX_VALUE);
         for (int i = 0; i < width; i++) {
-            Arrays.fill(distTo[i], Integer.MAX_VALUE);
-            distTo[i][0] = energyArray[i][0];
+            distTo[i] = energyArray[i];
         }
 
         int newDist = 0;
@@ -416,10 +314,10 @@ public class Carver {
 
                 // For each of the current node's children, check if the path through the current node is shorter
                 for (int i = lower; i < upper + 1; i++) {
-                    newDist = distTo[x][y] + energyArray[x+i][y+1];
-                    if (newDist < distTo[x+i][y+1]) {
-                        distTo[x+i][y+1] = newDist;
-                        parent[x+i][y+1] = x;
+                    newDist = distTo[y*width + x] + energyArray[(y+1)*width + x+i];
+                    if (newDist < distTo[(y+1)*width+x+i]) {
+                        distTo[(y+1)*width+x+i] = newDist;
+                        parent[(y+1)*width+x+i] = x;
                     }
                 }
             }
@@ -427,13 +325,13 @@ public class Carver {
 
         // Backtracking from the minimum of the last row:
         // Finding minimum of the last row:
-        int minEnergy = distTo[0][height-1];
+        int minEnergy = distTo[(height-1)*width];
         int minX = 0;
 
         for (int x = 1; x < width; x++) {
-            if (distTo[x][height-1] < minEnergy) {
+            if (distTo[(height-1)*width+x] < minEnergy) {
                 minX = x;
-                minEnergy = distTo[x][height-1];
+                minEnergy = distTo[(height-1)*width+x];
             }
         }
 
@@ -445,7 +343,7 @@ public class Carver {
 
         int parentX;
         for (int y = height - 1; y > 0; y--) {
-            parentX = parent[childX][y];
+            parentX = parent[y*width+childX];
             minPath[y-1] = parentX;
             childX = parentX;
         }
