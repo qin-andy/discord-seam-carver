7c447426cbddc21323bd34f299ad19fd91de0964
andyq <andyqin17@gmail.com>
2021-04-01 21:31:38 -0700

    Adjusted comments on PathRemover

--- src/main/java/PathRemover.java
+++ src/main/java/PathRemover.java
@@ -7,6 +7,8 @@ public class PathRemover {
         initialWidth = iWidth;
     }
 
+    // Takes a BufferedImage and a vertical seam defined in a 1d array of X values
+    // Returns a new BufferedImage with the vertical seam removed
     public BufferedImage removePath(BufferedImage image, int[] path) {
         int width = image.getWidth();
         int height = image.getHeight();
