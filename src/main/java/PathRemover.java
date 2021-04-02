abdbcf85ac78088d2dd1fad610b13113d15a6c97
andyq <andyqin17@gmail.com>
2021-04-01 21:29:59 -0700

    Moved path removal to its own class

---
+++ src/main/java/PathRemover.java
@@ -0,0 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class PathRemover {
+    private int initialWidth;
+    public PathRemover(int iWidth) {
+        initialWidth = iWidth;
+    }
+
+    public BufferedImage removePath(BufferedImage image, int[] path) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = path[y]; x < image.getWidth() - 1; x++) {
+                int position = y*initialWidth + x;
+                pixels[position] = pixels[position+1];
+            }
+        }
+        return image.getSubimage(0, 0, image.getWidth() - 1, image.getHeight());
+    }
+}
